3226ec87db7ac3e43a3862c80eaa478b7b436db9
zhouxiao <zhoux1071@163.com>
2020-11-04 14:13:05 +0800

    no message

---
+++ Test3/main.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "calc/Calc.h"
+
+int main(void)
+{
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "1 + 1 = " << add(1, 1);
+    
+}
